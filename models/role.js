e724c35c496abe76ce7d304f0df20ec10718e519
hrithik812 <hrithik08.rudra@gmail.com>
2024-11-27 08:30:01 +0600

    Dashboard first commit

---
+++ models/role.js
@@ -0,0 +1,31 @@
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Role extends Model {
+    static associate(models) {
+      // Define associations here
+    }
+  }
+  Role.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.STRING,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Role',
+    }
+  );
+  return Role;
+};
