5e46d35357317fd7fc592f844569c9a439966d50
hrithik812 <hrithik08.rudra@gmail.com>
2024-11-28 08:02:01 +0600

    Feature creation done

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,43 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const { User } = require('../models');
+const { User,Feature,Role } = require('../models');
 const router = express.Router();
 const bcrypt = require('bcryptjs'); // Import bcrypt
 
 const SECRET_KEY = '123456';
+const getUserFeatures = async (username) => {
+  try {
+    const user = await User.findOne({
+      where: { username },
+      include: [
+        {
+          model: Role,
+          through: { attributes: [] }, // Exclude intermediate table attributes
+          include: [
+            {
+              model: Feature,
+              through: { attributes: [] }, // Exclude intermediate table attributes
+            },
+          ],
+        },
+      ],
+    });
 
+    if (!user) {
+      return { message: 'User not found', features: [] };
+    }
+
+    
+
+    // Extract features
+    const features = user.Roles.flatMap((role) => role.Features);
+
+    return { username, features };
+  } catch (error) {
+    console.error('Error fetching features:', error);
+    throw new Error('Failed to fetch features');
+  }
+};
 // User Registration
 router.post('/register', async (req, res) => {
     const { username, email, password } = req.body;
@@ -15,6 +47,9 @@ router.post('/register', async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, salt); // Hash the password
 
     const user = await User.create({ username, email, password:hashedPassword });
+  
+    // Example usage
+
     res.status(201).json({ message: 'User created successfully', user });
   } catch (err) {
     res.status(500).json({ error: err.message });
@@ -31,17 +66,20 @@ router.post('/login', async (req, res) => {
     if (!user) {
         return res.status(404).json({ message: 'User not found' });
       }
-  
+
+      
       // Compare the provided password with the stored hashed password
       const isMatch = await bcrypt.compare(password, user.password);
       if (!isMatch) {
         return res.status(401).json({ message: 'Invalid credentials' });
       }
-
+      const data = await getUserFeatures(user.username);
+      
+      
     const token = jwt.sign({ id: user.id, role: user.role }, SECRET_KEY, {
       expiresIn: '1h',
     });
-    res.json({ message: 'Login successful', token });
+    res.json({ message: 'Login successful', token,feature:data });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
